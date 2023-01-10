aed6d7cb7107c59b75549f6bd117b358ccac33a4
T-pl <tiagofqu@gmail.com>
2023-01-10 19:47:48 -0300

    feat(pokemons-page): Finalizei a Pokemon Page trazendo os dados via API

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from '@mui/material'
+import { Button, Container, Grid, Box } from '@mui/material'
 import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
 import Banner from '../../assets/pikachu-banner.png'
@@ -10,26 +10,27 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <Container maxWidth="xl">
-        <main>
-          <div className='right-side'>
-            <h1>Encontre os seus <b>Pokémons</b> preferidos!</h1>
-            <p>Conheça os diferentes tipos de Pokémons, suas habilidades e muito mais.</p>
-            <Button variant="contained">
-              <Link
-                to={'/pokemons'}
-                style={{ color: '#fff', textDecoration: 'none' }}
-              >
-                Encontrar Pokémons
-              </Link>
-            </Button>
-          </div>
-          <div className='left-side'>
-            <img src={Banner} alt="Banner Pikachu" />
-          </div>
-        </main>
+      <Container maxWidth="lg">
+        <Box mt={5}>
+          <Grid container>
+            <Grid item xs={12} lg={6}>
+              <h1>Encontre os seus <b>Pokémons</b> preferidos!</h1>
+              <p>Conheça os diferentes tipos de Pokémons, suas habilidades e muito mais.</p>
+              <Button variant="contained">
+                <Link
+                  to={'/pokemons'}
+                  style={{ color: '#fff', textDecoration: 'none' }}
+                >
+                  Encontrar Pokémons
+                </Link>
+              </Button>
+            </Grid>
+            <Grid alignItems={"center"} item xs={6} lg={6} >
+              <img src={Banner} alt="Banner Pikachu" />
+            </Grid>
 
-
+          </Grid>
+        </Box>
       </Container>
     </div>
   )
