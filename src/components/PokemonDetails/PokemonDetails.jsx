aa8b570e407a2d60538fb1aaa7f93c7d80464e81
T-pl <tiagofqu@gmail.com>
2023-01-11 10:25:54 -0300

    feat(details-page): Fiz ajustes na página de detalhes do pokémon e também no README

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -3,61 +3,75 @@ import { useEffect, useState } from 'react';
 import Axios from 'axios';
 import Navbar from '../Navbar/Navbar'
 import { useParams } from 'react-router-dom';
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
+import { Container } from '@mui/system';
+import './PokemonDetails.css'
 
 export default function PokemonDetails() {
   const { name } = useParams();
   const [selectPokemonDetail, setPokemonDetail] = useState({});
-
   useEffect(() => {
     if (!name) return;
     Axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
       .then((res) => setPokemonDetail(res.data))
   }, [name]);
 
-  useEffect(() => {
-    console.log('details', selectPokemonDetail)
-  }, [selectPokemonDetail])
 
   return (
     <>
-      <Navbar
-      />
-      <Box m={2} >
-        {selectPokemonDetail.sprites ? <img
-          width='100%' src={selectPokemonDetail.sprites.front_default} alt={name} /> : null}
-        <Box display='flex' flex-direction='row' gap={1}>
-          <Typography variant='h2'>
-            {selectPokemonDetail.name}
-          </Typography>
-        </Box>
-        <Box display='flex' flex-direction='row' gap={1}>
-          <Typography>Altura:</Typography>
-          <Typography>
-            {selectPokemonDetail.height}
-          </Typography>
-        </Box>
-        <Box display='flex' flex-direction='row' gap={1}>
-          <Typography>
-            Peso:
-          </Typography>
-          <Typography>
-            {selectPokemonDetail.weight}
-          </Typography>
-          {/* {Boolean(selectPokemonDetail.stats) && selectPokemonDetail.stats.map((item) => {
-            return (
-              <>
+      <Navbar />
+      <Container maxWidth='lg' >
+        <Box mt={5}>
+          <Grid container>
+            <Grid item xs={12} lg={6}>
+              {selectPokemonDetail.sprites ? <img id='image-details'
+                width='100%' src={selectPokemonDetail.sprites.front_default} alt={name} /> : null}
+            </Grid>
+            <Grid item xs={12} lg={5} ml={5}>
+              <Box display='flex' flex-direction='row' gap={1}>
+                <Typography variant='h4'>
+                  {selectPokemonDetail.name}
+                </Typography>
+              </Box>
+              <Box display='flex' flex-direction='row' gap={1}>
+                <Typography sx={{ fontWeight: 'bold' }}>Altura:</Typography>
                 <Typography>
-                  STATS:
+                  {selectPokemonDetail.height}
+                </Typography>
+              </Box>
+              <Box display='flex' flex-direction='row' gap={1}>
+                <Typography sx={{ fontWeight: 'bold' }}>
+                  Peso:
                 </Typography>
                 <Typography>
-                  {item.base_stat}
+                  {selectPokemonDetail.weight}
                 </Typography>
-              </>
-            )
-          })} */}
+              </Box>
+              <Box display='flex' flex-direction='row' gap={1}>
+                <Typography sx={{ fontWeight: 'bold' }}>
+                  Tipo:
+                </Typography>
+                <Typography >
+                  {selectPokemonDetail.types ? selectPokemonDetail.types.map((type) => type.type.name.replace(/^./, (str) => str.toUpperCase())) : null}
+                </Typography>
+
+              </Box>
+              <Box display='flex' flex-direction='row' gap={1}>
+                <Typography sx={{ fontWeight: 'bold' }}>
+                  Habilidades:
+                </Typography>
+                <Typography >
+                  {selectPokemonDetail.abilities ? selectPokemonDetail.abilities.map((ability) => ability.ability.name + ', ') : null}
+                </Typography>
+              </Box>
+              {selectPokemonDetail.stats ? selectPokemonDetail.stats.map((stat) => stat.base_state) : null}
+
+              {console.log(selectPokemonDetail)}
+            </Grid>
+          </Grid>
         </Box>
-      </Box>
+      </Container>
+
     </>
   )
 }
--- src/themes/themes.jsx
+++
@@ -1,7 +0,0 @@
-import { createTheme } from "@mui/system";
-
-export const ThemeCustom = createTheme({
-  palette: {
-
-  }
-})
